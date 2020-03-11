9e032ce9f8a763dbf148404332d00d5ef272be51
chinmaydash007 <chinmaydash11@gmail.com>
2020-03-11 23:45:55 +0530

    Initail Commit

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -11,12 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        var gson: Gson = Gson()
-//        var article1 = Article("chinmay",null,null)
-//        var a: String = gson.toJson(article1)
-//        Log.d("mytag", a)
-//        var article: Article = gson.fromJson(a, Article::class.java)
-//        article.title="lsnadna"
+
 
     }
 }
