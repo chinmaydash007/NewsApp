77d497eb9f1380f69ecc1b9d426c9ae30cd0d2ea
chinmaydash007 <chinmaydash11@gmail.com>
2020-03-11 23:49:52 +0530

    Initail Commit

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -2,9 +2,6 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.example.newsapp.Model.Article
-import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/com/example/newsapp/Model/Article.kt
+++ app/src/main/java/com/example/newsapp/Model/Article.kt
@@ -1,24 +1,8 @@
 package com.example.newsapp.Model
 
-import com.google.gson.annotations.SerializedName
+import javax.xml.transform.Source
 
-data class Article(
-    @SerializedName("title") private var _title: String?,
-    @SerializedName("body") private val _body: String? = "",
-    val viewCount: Int = 0,
-    val payWall: Boolean = false,
-    @SerializedName("titleImage") private val _titleImage: String? = ""
-) {
-    var title
-        get() = _title ?: throw IllegalArgumentException("Title is required")
-    set(value){
-        _title=value
-    }
-    val body
-        get() = _body ?: ""
-    val titleImage
-        get() = _titleImage ?: ""
-    init {
-        this.title
-    }
+class Article {
+    var source:Source?=null
+
 }
